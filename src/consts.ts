cc9dcad50e5f12e09006710ca80c3a5f84622bf9
Nick French <64120654+nickjfrench@users.noreply.github.com>
2026-01-31 18:56:44 -0500

    Stripping out styles and moving to Tailwind theme and playing with index

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'nickfrench.dev';
+export const SITE_DESCRIPTION = 'Full-stack and applied LLM dev, with a sprinkle of game dev.';
